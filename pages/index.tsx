79b90d8ccd8306a575a00cc86ff6540f29f3763a
Bogdan Preda <bogdan.preda@themeisle.com>
2022-02-01 23:47:15 +0200

    chore cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,13 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
   )
 }
 
